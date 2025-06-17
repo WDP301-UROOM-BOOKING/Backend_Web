628bffe18c4fa412e5c398074acf6a5da8928454
Nguyen Duc Thinh <thinhnnguyenndt248@gmail.com>
2025-06-17 14:08:49 +0700

    ducthinh-rebase-origin/main

--- src/middlewares/checkCustomer.js
+++ src/middlewares/checkCustomer.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const checkCustomer = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
+  console.log("token >> ", token)
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
--- src/route_controller/Payment/PaymentRoute.js
+++ src/route_controller/Payment/PaymentRoute.js
@@ -4,11 +4,17 @@ const PaymentRouter = express.Router();
 const PaymentController = require("./PaymentController");
 const checkCustomer = require("../../middlewares/checkCustomer");
 const checkOwner = require("../../middlewares/checkOwner");
+// const bodyParser = require("body-parser");
 
 PaymentRouter.post(
   "/create-booking",
   checkCustomer,
   PaymentController.createBooking
+);
+PaymentRouter.post(
+  "/checkout-booking",
+  checkCustomer,
+  PaymentController.checkoutBooking
 );
 PaymentRouter.post(
   "/cancel-payment",
@@ -25,5 +31,12 @@ PaymentRouter.get(
   checkOwner,
   PaymentController.getReservations
 );
+
+// Webhook Stripe phải dùng raw body
+// PaymentRouter.post(
+//   "/api/payment/webhook",
+//   bodyParser.raw({ type: "application/json" }),
+//   PaymentController.stripeWebhookHandler
+// )
 
 module.exports = PaymentRouter;
